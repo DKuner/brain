b63529902a7793f587e09f5f782f72240e5e69d0
liwensheng <liwensheng@localhost.(none)>
2012-07-09 10:31:20 +0800

    Initial Commit

---
+++ HZGame/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  HZGame
+//
+//  Created by liwensheng on 12-7-9.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
